f27d360fe8dd684be5b7412a9938c74df5ee076d
Ricardo Brambila <ricardo.brambila.jsx@gmail.com>
2019-05-24 15:24:49 -0300

    fix(Loader): uses the same size of the map

--- src/components/Loader/MapsLoader.js
+++ src/components/Loader/MapsLoader.js
@@ -8,9 +8,7 @@ const MapsLoader = () => {
   const isWide = useMediaLayout({ minWidth: 500 });
 
   return isWide ? (
-    <ContentLoader height={400} width={400} speed={2} primaryColor="#b6b6b6" secondaryColor="#c0bcbc">
-      <rect data-testid="maps-rect" x="0" y="0" rx="0" ry="0" width="180" height="100" />
-    </ContentLoader>
+    <ContentLoader height={300} width={500} speed={2} primaryColor="#b6b6b6" secondaryColor="#c0bcbc" />
   ) : (
     <SmallLoader>The map is loading...</SmallLoader>
   );
