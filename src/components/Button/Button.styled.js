4ec2799aca70bf33537b9ebed3f52107573138c3
Ricardo Brambila <ricardo.brambila.jsx@gmail.com>
2019-05-24 01:05:01 -0300

    feat(Button): creates the component

---
+++ src/components/Button/Button.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const StyledButton = styled.button`
+  background-color: #448cff;
+  border-radius: 4px;
+  border: none;
+  color: white;
+  cursor: pointer;
+  font-weight: 600;
+  padding: 8px 22px;
+  text-decoration: none;
+`;
