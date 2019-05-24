7894076ec2569fa7d89f51f8c77ebdc79c2b95ee
Ricardo Brambila <ricardo.brambila.jsx@gmail.com>
2019-05-24 00:41:09 -0300

    style(Close): aligns as design

--- src/components/Close/Close.styled.js
+++ src/components/Close/Close.styled.js
@@ -3,12 +3,13 @@ import styled from 'styled-components';
 export const CloseContainer = styled.div`
   background-color: white;
   border: 2px solid #cecece;
-  padding: 4px;
   width: 100%;
+  padding: 16px 32px;
+  position: relative;
 
   > svg {
     position: absolute;
-    right: 0;
+    right: 16px;
 
     &:hover {
       cursor: pointer;
