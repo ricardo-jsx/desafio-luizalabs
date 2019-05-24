275aae0a978ca24a2fab6116fa2fa9e24f2c1636
Ricardo Brambila <ricardo.brambila.jsx@gmail.com>
2019-05-24 20:24:30 -0300

    Updates

--- src/components/Input/Input.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-
-import Input from './Input';
-
-storiesOf('Input', module).add('with cep mask', () => (
-  <Input label="CEP" mask="11111-111" onChange={action('onChange')} />
-));
