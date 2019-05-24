7385e05f09bd0023d860edb50a9a2642cb80c878
Ricardo Brambila <ricardo.brambila.jsx@gmail.com>
2019-05-24 00:41:09 -0300

    style(Close): aligns as design

--- src/components/Close/Close.stories.js
+++ src/components/Close/Close.stories.js
@@ -3,8 +3,10 @@ import { storiesOf } from '@storybook/react';
 
 import Close from './Close';
 
-storiesOf('Close', module).add('default', () => (
-  <Close>
-    <div>If you are reading this, I wish you have a nice day :3</div>
-  </Close>
-));
+storiesOf('Close', module)
+  .addDecorator((s) => <div style={{ width: '80%' }}>{s()}</div>)
+  .add('default', () => (
+    <Close>
+      <div>If you are reading this, I wish you have a nice day :3</div>
+    </Close>
+  ));
