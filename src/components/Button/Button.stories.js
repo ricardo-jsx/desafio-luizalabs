07b37b2b4e251b4e17c29fde2fc9a27ac6b52839
Ricardo Brambila <ricardo.brambila.jsx@gmail.com>
2019-05-24 01:12:45 -0300

    docs(Button): adds a more complex story

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -3,5 +3,15 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import Button from './Button';
+import Input from '../Input';
 
-storiesOf('Button', module).add('default', () => <Button onClick={action('click')}>Buscar</Button>);
+storiesOf('Button', module)
+  .add('default', () => <Button onClick={action('click')}>Buscar</Button>)
+  .add('inside form', () => (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <Input label="CEP" mask="11111-111" onChange={action('onChange')} />
+      <Button type="submit" onClick={action('submit')} style={{ marginLeft: '8px' }}>
+        Buscar
+      </Button>
+    </form>
+  ));
