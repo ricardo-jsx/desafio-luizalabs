d730d5a6949863cfdebd341739177bbc72a40b94
Ricardo Brambila <ricardo.brambila.jsx@gmail.com>
2019-05-24 00:49:36 -0300

    test(Input): removes async declaration

--- src/components/Input/Input.spec.js
+++ src/components/Input/Input.spec.js
@@ -4,7 +4,7 @@ import { render, fireEvent } from 'react-testing-library';
 import Input from './Input';
 
 describe('Input', () => {
-  it('should allow only the characters of the mask', async () => {
+  it('should allow only the characters of the mask', () => {
     const onChange = jest.fn();
     const { getByLabelText } = render(<Input id="input" mask="111" onChange={onChange} />);
 
